17aefd5d2f9ed36096b8bd92101692c0853b3cf0
Rofiat <oyizakareem9@gmail.com>
2024-11-22 01:04:56 +0100

    lint checks repair

--- travel_agency_analysis.py
+++ travel_agency_analysis.py
@@ -13,19 +13,24 @@ db_password = os.getenv('DB_PASSWORD')
 
 # Database connection details
 db_user = "rofee"
-db_host = "terraform-20241113122124605100000001.cz4qw448wzk4.us-east-1.rds.amazonaws.com"
+db_host = (
+    "terraform-20241113122124605100000001."
+    "cz4qw448wzk4.us-east-1.rds.amazonaws.com"
+)
 db_port = "5432"
 db_name = "travel_agency_dw"
 
 # Connection string
-connection_string = f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+connection_string =(
+    f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:"
+    f"{db_port}/{db_name}"
+)
 
 # Create the engine
 engine = create_engine(connection_string)
 
 # Query to fetch all views from the database
-query_views = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'VIEW';"
-
+query_views = "SELECT table_name FROM pg_catalog.pg_views WHERE schemaname = 'public';"
 # Fetch list of all views
 views_df = pd.read_sql(query_views, engine)
 print("Available Views:")
@@ -39,11 +44,19 @@ for view in views_df['table_name']:
     print(data.head())  # Preview first 5 rows of each view
 
 # Fetch the data for currency_count_by_continent
-currency_data = pd.read_sql("SELECT * FROM public.currency_count_by_continent;", engine)
-
+currency_data = pd.read_sql(
+    "SELECT * FROM public."
+    "currency_count_by_continent;", engine
+)
 # Save Bar Plot for currency_count_by_continent
 plt.figure(figsize=(10, 6))
-sns.barplot(x='continent', y='total_currencies', data=currency_data, hue='continent', palette="Set2")
+sns.barplot(
+    x='continent',
+    y='total_currencies',
+    data=currency_data,
+    hue='continent',
+    palette="Set2"
+)
 plt.title("Number of Currencies by Continent")
 plt.xlabel("Continent")
 plt.ylabel("Number of Currencies")
@@ -51,15 +64,30 @@ plt.savefig("currency_count_by_continent.png")
 plt.close()
 
 # Fetch the data for population_density
-population_density_data = pd.read_sql("SELECT * FROM public.population_density;", engine)
+population_density_data = pd.read_sql(
+    "SELECT * FROM public."
+    "population_density;", engine
+)
 
 # Sorting data to get the top 10 highest and lowest population density
-top_population_density = population_density_data.sort_values(by='population_density', ascending=False).head(10)
-bottom_population_density = population_density_data.sort_values(by='population_density', ascending=True).head(10)
+top_population_density = population_density_data.sort_values(
+    by='population_density',
+    ascending=False
+).head(10)
+bottom_population_density = population_density_data.sort_values(
+    by='population_density',
+    ascending=True
+).head(10)
 
 # Plot for Top 10 Countries by Population Density
 plt.figure(figsize=(12, 6))
-sns.barplot(x='country_name', y='population_density', data=top_population_density, palette="Greens_d")
+sns.barplot(
+    x='country_name',
+    y='population_density',
+    data=top_population_density,
+    hue='country_name',
+    palette="Greens_d"
+)
 plt.title("Top 10 Countries by Population Density")
 plt.xlabel("Country")
 plt.ylabel("Population Density")
@@ -70,7 +98,13 @@ plt.close()
 
 # Plot for Bottom 10 Countries by Population Density
 plt.figure(figsize=(12, 6))
-sns.barplot(x='country_name', y='population_density', data=bottom_population_density, palette="Reds_d")
+sns.barplot(
+    x='country_name',
+    y='population_density',
+    data=bottom_population_density,
+    hue='country_name',
+    palette="Reds_d"
+)
 plt.title("Bottom 10 Countries by Population Density")
 plt.xlabel("Country")
 plt.ylabel("Population Density")
@@ -80,11 +114,20 @@ plt.savefig("bottom_10_population_density.png")
 plt.close()
 
 # Fetch the data for start_of_week_analysis
-start_of_week_data = pd.read_sql("SELECT * FROM public.start_of_week_analysis;", engine)
+start_of_week_data = pd.read_sql(
+    "SELECT * FROM public."
+    "start_of_week_analysis;", engine
+)
 
 # Save Bar Plot for start_of_week_analysis
 plt.figure(figsize=(8, 5))
-sns.barplot(x='start_of_week', y='country_count', data=start_of_week_data, hue='start_of_week', palette="Blues_d")
+sns.barplot(
+    x='start_of_week',
+    y='country_count',
+    data=start_of_week_data,
+    hue='start_of_week',
+    palette="Blues_d"
+)
 plt.title("Start of the Week Analysis")
 plt.xlabel("Day of the Week")
 plt.ylabel("Country Count")
@@ -96,13 +139,27 @@ regions_data = pd.read_sql("SELECT * FROM public.top_regions;", engine)
 fig, axes = plt.subplots(1, 2, figsize=(18, 6))
 
 # Population plot
-sns.barplot(x='region', y='total_population', data=regions_data, ax=axes[0], hue='region', palette="Blues_d")
+sns.barplot(
+    x='region',
+    y='total_population',
+    data=regions_data,
+    ax=axes[0],
+    hue='region',
+    palette="Blues_d"
+)
 axes[0].set_title("Total Population by Region")
 axes[0].set_xlabel("Region")
 axes[0].set_ylabel("Total Population")
 
 # Area plot
-sns.barplot(x='region', y='total_area', data=regions_data, ax=axes[1], hue='region', palette="Greens_d")
+sns.barplot(
+    x='region',
+    y='total_area',
+    data=regions_data,
+    ax=axes[1],
+    hue='region',
+    palette="Greens_d"
+)
 axes[1].set_title("Total Area by Region")
 axes[1].set_xlabel("Region")
 axes[1].set_ylabel("Total Area (sq km)")
@@ -112,11 +169,20 @@ plt.savefig("total_population_and_area_by_region.png")
 plt.close()
 
 # Fetch the data for top_languages_by_region
-languages_data = pd.read_sql("SELECT * FROM public.top_languages_by_region;", engine)
+languages_data = pd.read_sql(
+    "SELECT * FROM public."
+    "top_languages_by_region;", engine
+)
 
 # Save Bar Plot for top_languages_by_region
 plt.figure(figsize=(12, 6))
-sns.barplot(x='region', y='language_count', data=languages_data, hue='languages', palette="Set1")
+sns.barplot(
+    x='region',
+    y='language_count',
+    data=languages_data,
+    hue='languages',
+    palette="Set1"
+)
 plt.title("Top Languages by Region")
 plt.xlabel("Region")
 plt.ylabel("Language Count")
